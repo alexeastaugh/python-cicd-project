fd35c47839849237febd10b08e3fc28d0d49e2f7
Alex Eastaugh <alex@akoova.com>
2024-03-01 18:10:28 +0000

    Correcting flake8 lint warnings

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 def add_numbers(a, b):
     return a + b
 
+
 if __name__ == '__main__':
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 # Test for app.py
-
 import unittest
 from app import add_numbers
+
 
 class TestAddNumbers(unittest.TestCase):
     def test_add_numbers(self):
@@ -10,6 +10,6 @@ class TestAddNumbers(unittest.TestCase):
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(-1, -1), -2)
 
+
 if __name__ == '__main__':
     unittest.main()
-    
